3c72a40bee165b7126261917cd2f2cf8e03a4b13
qamichaelmaia <contatomichaelmaia@gmail.com>
2023-12-05 20:33:22 -0300

    last commit

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -5,7 +5,7 @@ import {
 } from '@faker-js/faker';
 
 
-context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
+describe('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
         Quero acessar a Loja EBAC 
         Para fazer um pedido de 4 produtos 
@@ -20,7 +20,7 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         let end1 = faker.address.streetName();
         let end2 = faker.address.buildingNumber();
         let cidade = faker.address.cityName();
-        let estado = 'Santa Catarina';
+        let estado = 'Bahia';
         let zip = faker.address.zipCode('########');
         let fone = faker.phone.number('+55 ## ##### ####');
         let email = faker.internet.email(nome);
@@ -34,24 +34,26 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
             cy.get('[class="product-block grid"]').first().click()
         });
 
-        it('Deve adicionar 4 produtos ao carrinho com command custom ', () => {
+        it('Deve adicionar 4 produtos ao carrinho com command custom - Deve preencher o cadastro de faturamento - Deve realizar os complementos do cadastro e finalizar a compra', () => {
             cy.addProdutos('Beaumont Summit Kit', 'XL', 'Red', 2)
             cy.get('.woocommerce-message').should('contain', '× “Beaumont Summit Kit” foram adicionados no seu carrinho.')
             cy.get('#primary-menu > .menu-item-629 > a').click()
             
             cy.addProdutos('Abominable Hoodie', 'M', 'Green', 2)
             cy.get('.woocommerce-message').should('contain', '× “Abominable Hoodie” foram adicionados no seu carrinho.')
-    
-        });
+            
+            cy.visit('checkout/')
 
-        it('Deve preencher o cadastro de faturamento', () => {
+            //Deve preencher o cadastro de faturamento
+
             EnderecoPage.editarEnderecoFaturamento(nome, sobrenome, empresa, pais, end1, end2, cidade, estado, zip, fone, email)
-    });
 
-        it('Deve realizar os complementos do cadastro e finalizar a compra', () => {
+            //'Deve realizar os complementos do cadastro e finalizar a compra'
+            
             cy.get('#order_comments').click().type("Plataforma prática e com estilos variados.")
             cy.get('#terms').click()
             cy.get('#place_order').click()
             cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+    
         });
 })
--- cypress/support/page_objects/endereco.page.js
+++ cypress/support/page_objects/endereco.page.js
@@ -1,29 +1,17 @@
 class EnderecoPage {
 
-    editarEnderecoFaturamento(nome, sobrenome, empresa, pais, enredeco, numero, cidade, estado, cep, telefone, email) {
-        //elementos + ações
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
-        cy.get(':nth-child(1) > .title > .edit').click()
-        //nome e sobrenome
+    editarEnderecoFaturamento(nome, sobrenome, empresa, pais, end1, end2, cidade, estado, zip, fone, email) {
         cy.get('#billing_first_name').clear().type(nome)
         cy.get('#billing_last_name').clear().type(sobrenome)
-        //instituição
         cy.get('#billing_company').clear().type(empresa)
-        //país (interação de elemento) 
-        cy.get('#select2-billing_country-container').click().type(pais).get('[aria-selected="true"]').click()
-        //Endereço
-        cy.get('#billing_address_1').clear().type(enredeco)
-        cy.get('#billing_address_2').clear().type(numero)
+        cy.get('#select2-billing_country-container').click().type(pais + '{enter}')
+        cy.get('#billing_address_1').clear().type(end1)
+        cy.get('#billing_address_2').clear().type(end2)
         cy.get('#billing_city').clear().type(cidade)
-        //Estado + cep
         cy.get('#select2-billing_state-container').click().type(estado + '{enter}')
-        cy.get('#billing_postcode').clear().type(cep)
-        //Telefone
-        cy.get('#billing_phone').clear().type(telefone)
-        //Email
+        cy.get('#billing_postcode').clear().type(zip)
+        cy.get('#billing_phone').clear().type(fone)
         cy.get('#billing_email').clear().type(email)
-        //Botão
-        cy.get(':nth-child(2) > .button').click()
     }
 
     editarEnderecoEntrega(nome, sobrenome, empresa, pais, endereco, numero, cidade, estado, cep) {
